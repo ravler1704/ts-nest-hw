0edda5e2bd282e003a3bd428e610a35fb473fd53
Alexandr <raskatov.av@mipt.ru>
2024-07-10 11:20:52 +0300

    1_2 fix after check

--- 1_2_Система_типов_TypeScript_Принципы_ООП_SOLID/src/index.ts
+++ 1_2_Система_типов_TypeScript_Принципы_ООП_SOLID/src/index.ts
@@ -11,12 +11,12 @@ interface IBook {
   id: string,
 }
 
-abstract class BooksRepository {
-  abstract createBook(book): boolean
-  abstract getBook(id): IBook
-  abstract getBooks(): IBook[]
-  abstract updateBook(id): boolean
-  abstract deleteBook(id): boolean
+export abstract class BooksRepository {
+  abstract createBook(book: any): Promise<any>;
+  abstract getBook(id: string): Promise<any>;
+  abstract getBooks(): Promise<any>;
+  abstract updateBook(id: string, book: any): Promise<any>;
+  abstract deleteBook(id: string): Promise<any>;
 }
 
 class Book {
